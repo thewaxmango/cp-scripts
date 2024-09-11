b6736a811bf2e238bb54ebdc7321945f1231856b
thewaxmango <77765712+thewaxmango@users.noreply.github.com>
2024-09-11 17:08:37 -0400

    Update temp.py

--- templates/temp.py
+++ templates/temp.py
@@ -1,12 +1,19 @@
 from sys import stdin, stdout
-I = lambda: stdin.readline().rstrip("\r\n")
-II = lambda: int(I())
-IL = lambda: I().split()
-IIL = lambda: [int(x) for x in IL()]
-IM = lambda: map(int, IL())
-P = lambda s: stdout.write(str(s))
-PL = lambda s: stdout.write(str(s) + "\n")
-F = lambda: stdout.flush()
+
+def I(): return stdin.readline().strip(" \r\n")
+def II(): return int(I())
+def IL(): return I().split()
+def ICL(): return list(I())
+def IIL(): return list(map(int, IL()))
+def IM(): return map(str, IL())
+def IIM(): return map(int, IL())
+def ICLM(): return map(int, ICL())
+def P(*args,sep=' ',end=''): stdout.write(sep.join([str(s) for s in args]) + end)
+def PL(*args,sep=' '): P(*args, sep=sep, end='\n')
+def PLi(arg,end='\n'): P(*arg, sep='\n', end=end)
+def F(): stdout.flush()
+def Y(): PL("YES")
+def N(): PL("NO")
 
 def main():
     for _ in range(II()):
