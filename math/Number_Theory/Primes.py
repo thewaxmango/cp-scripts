9308c09caf57b7db216e03e033e99b8d46ee61af
thewaxmango <77765712+thewaxmango@users.noreply.github.com>
2025-09-11 15:47:12 -0400

    Update Primes.py

--- math/Number_Theory/Primes.py
+++ math/Number_Theory/Primes.py
@@ -4,3 +4,18 @@ def primes_to(n):
         if sieve[i//2]:
             sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
     return [2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
+
+def linear_sieve(n):
+    lp = [0]*(n+1)
+    pr = []
+    for i in range(2, n+1):
+        if lp[i] == 0:
+            lp[i] = i
+            pr.append(i)
+        j = 0
+        while i * pr[j] <= n:
+            lp[i * pr[j]] = pr[j]
+            if pr[j] == lp[i]:
+                break
+            j += 1
+    return lp, pr
